6aeb62dbbb94ce3f682dbbd12adea6cb67c8cfff
I-Adze <adam.ward1202@gmail.com>
2023-01-15 14:17:12 +0000

    Adds React router and empty login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter, Navigate } from "react-router-dom";
 import "./App.scss";
+import { LoginPage } from "./login/LoginPage";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login"></Navigate>}></Route>
+            <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
