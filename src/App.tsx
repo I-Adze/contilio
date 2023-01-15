c38e8a1bd5a973484190b8b376a1bd28877111b7
I-Adze <adam.ward1202@gmail.com>
2023-01-15 16:03:29 +0000

    Adds styling for login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import { BrowserRouter, Navigate } from "react-router-dom";
 import "./App.scss";
+import { Dashboard } from "./dashboard/Dashboard";
 import { LoginPage } from "./login/LoginPage";
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
           <Routes>
             <Route path="/" element={<Navigate to="/login"></Navigate>}></Route>
             <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+            <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
           </Routes>
         </main>
       </BrowserRouter>
--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -1,28 +1,67 @@
 import React from "react";
 import { NavigateFunction } from "react-router-dom";
 import { withRouter } from "../shared/withRouter";
+import styles from "./LoginPage.module.scss";
 
 interface LoginPageProps {
   navigate: NavigateFunction;
 }
-class LoginPageInternal extends React.Component<LoginPageProps> {
+
+interface LoginPageState {
+  username: string;
+  password: string;
+}
+
+class LoginPageInternal extends React.Component<
+  LoginPageProps,
+  LoginPageState
+> {
+  constructor(props: LoginPageProps) {
+    super(props);
+    this.state = { username: "", password: "" };
+  }
   handleSubmit() {
     this.props.navigate("/dashboard");
   }
 
   render() {
     return (
-      <form onSubmit={() => this.handleSubmit()}>
-        <label>
-          Username:
-          <input type="username" name="username" placeholder="Username" />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" placeholder="Password" />
-        </label>
-        <input type="submit" value="Login" />
-      </form>
+      <section className={styles.page}>
+        <form className={styles.form} onSubmit={() => this.handleSubmit()}>
+          <h2>Lorem Ipsum</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <label>
+            <input
+              type="username"
+              name="username"
+              placeholder="Username"
+              onChange={(e) =>
+                this.setState((state) => ({
+                  ...state,
+                  username: e.target.value,
+                }))
+              }
+            />
+          </label>
+          <label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={(e) =>
+                this.setState((state) => ({
+                  ...state,
+                  password: e.target.value,
+                }))
+              }
+            />
+          </label>
+          <input type="submit" value="Log in" />
+        </form>
+      </section>
     );
   }
 }
